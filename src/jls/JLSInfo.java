c956109d9191791323c07c82e2581a1b18c09337
bsiever <bsiever@gmail.com>
2024-06-11 15:05:41 -0500

    Generate compile time from compilation (not static date).

--- src/jls/JLSInfo.java
+++ src/jls/JLSInfo.java
@@ -2,6 +2,14 @@ package jls;
 
 import jls.sim.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Date;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
+
 import javax.help.*;
 
 /**
@@ -12,16 +20,14 @@ import javax.help.*;
 public final class JLSInfo { 
 	
 	// version info
-
-
-	public static final String build = "<p>[built May 28, 2024 at 5:30 PM]";
+	public static final String build = "<p>"+getClassBuildTime();
 	public static final int vers = 4;
-	public static final int release = 2;
-	public static final int buildNum = 5;
+	public static final int release = 4;
+	public static final int buildNum = 0;
 	public static final int year = 2014;
-	
-	public static final String version = "JLS " + vers + "." + release + 
-		" (Michigan Technological University / Updates by Bill Siever)";
+	public static final String authors = "David A. Poplawski (primary/original author);\n Joshua Marshall (contributor);\n Other MTU.EDU students,\n and Bill Siever (minor modifications and packaging)";
+	public static final String repository = "https://github.com/bsiever/JLS";
+	public static final String version = "JLS " + vers + "." + release;
 	
 	// miscellaneous parameters
 	public static final int windowsize = 600;
@@ -61,5 +67,39 @@ public final class JLSInfo {
 	 * Private constructor to keep this class from being instantiated.
 	 */
 	private JLSInfo() {}
+	
+	
+	// https://stackoverflow.com/questions/3336392/java-print-time-of-last-compilation
+	/**
+	 * Handles files, jar entries, and deployed jar entries in a zip file (EAR).
+	 * @return The date if it can be determined, or null if not.
+	 */
+	private static Date getClassBuildTime() {
+	    Date d = null;
+	    Class<?> currentClass = new Object() {}.getClass().getEnclosingClass();
+	    URL resource = currentClass.getResource(currentClass.getSimpleName() + ".class");
+	    if (resource != null) {
+	        if (resource.getProtocol().equals("file")) {
+	            try {
+	                d = new Date(new File(resource.toURI()).lastModified());
+	            } catch (URISyntaxException ignored) { }
+	        } else if (resource.getProtocol().equals("jar")) {
+	            String path = resource.getPath();
+	            d = new Date( new File(path.substring(5, path.indexOf("!"))).lastModified() );    
+	        } else if (resource.getProtocol().equals("zip")) {
+	            String path = resource.getPath();
+	            File jarFileOnDisk = new File(path.substring(0, path.indexOf("!")));
+	            //long jfodLastModifiedLong = jarFileOnDisk.lastModified ();
+	            //Date jfodLasModifiedDate = new Date(jfodLastModifiedLong);
+	            try(JarFile jf = new JarFile (jarFileOnDisk)) {
+	                ZipEntry ze = jf.getEntry (path.substring(path.indexOf("!") + 2));//Skip the ! and the /
+	                long zeTimeLong = ze.getTime ();
+	                Date zeTimeDate = new Date(zeTimeLong);
+	                d = zeTimeDate;
+	            } catch (IOException|RuntimeException ignored) { }
+	        }
+	    }
+	    return d;
+	}
 
 } // end of JLSInfo class
