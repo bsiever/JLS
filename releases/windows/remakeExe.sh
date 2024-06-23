cf30d56fb4e86d316eba01a17a24fbfde630cd10
Bill Siever <bsiever@gmail.com>
2024-06-22 22:03:08 -0500

    Updated windows build script.

--- releases/windows/remakeExe.sh
+++ releases/windows/remakeExe.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-jpackage --type app-image --app-version 4.4 --copyright ""  --description "J(ava) Logic Simulator" \
+rm -Rf JLS
+rm -Rf JLS_windows.zip
+
+rm contents/JLS.jar
+cp ../JLS.jar contents
+
+
+jpackage --type app-image --app-version 4.6 --copyright ""  --description "J(ava) Logic Simulator" \
     --name "JLS" \
     --icon  resources/JLS.ico  \
     --runtime-image ./jre/jdk-21.0.3+9-jre/ \
@@ -8,6 +15,5 @@ jpackage --type app-image --app-version 4.4 --copyright ""  --description "J(ava
     --main-jar JLS.jar \
     --main-class jls.JLS \
 
-
-
+tar -cvf JLS_windows.zip JLS
 
