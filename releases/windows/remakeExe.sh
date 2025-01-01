ec0cb55fe9cad95b386a51913f76820e643f0a51
bsiever <bsiever@gmail.com>
2025-01-01 12:59:01 -0600

    Updated remakeExe

--- releases/windows/remakeExe.sh
+++ releases/windows/remakeExe.sh
@@ -7,7 +7,7 @@ rm contents/JLS.jar
 cp ../JLS.jar contents
 
 
-jpackage --type app-image --app-version 4.9 --copyright ""  --description "J(ava) Logic Simulator" \
+jpackage --type app-image --app-version 4.10 --copyright ""  --description "J(ava) Logic Simulator" \
     --name "JLS" \
     --icon  resources/JLS.ico  \
     --runtime-image ./jre/jdk-21.0.3+9-jre/ \
@@ -15,7 +15,7 @@ jpackage --type app-image --app-version 4.9 --copyright ""  --description "J(ava
     --main-jar JLS.jar \
     --main-class jls.JLS \
 
-echo "Use file browser's Compress (Right-Click on JLS folder, Sent To > Compress", then rename file to JLS_windows.zip"
+echo "Use file browser's Compress (Right-Click on JLS folder, Sent To > Compress, then rename file to JLS_windows.zip"
 
 # powershell Compress-Archive JLS JLS_windows.zip
 #tar -cvf JLS_windows.zip JLS
